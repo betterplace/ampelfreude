2bdf799cd4caf77917e0f2748d1e3a6714649c6c
Daniel Hahn <dha@betterplace.org>
2011-06-10 11:29:53 +0200

    Fixed state check and logic

--- ampel_runner.rb
+++ ampel_runner.rb
@@ -4,21 +4,33 @@ require 'ampel_switcher'
 
 status_parser = CiStatusParser.new('http://192.168.1.4:8080/cc.xml', 'betterplace_ci')
 
-loop do 
-  if(status_parser.update && status_parser.state_changed?)
-    if(status_parser.build_running? && !status_parser.last_build_successful?)
-      AmpelSwitcher.green_on
-      AmpelSwitcher.red_on
-    elsif(status_parser.last_build_successful?)
-      AmpelSwitcher.green_on
-      AmpelSwitcher.red_off
-    else
-      AmpelSwitcher.red_on
-      AmpelSwitcher.green_off
-    end
+def set_ampel_state(status_parser)
+  return unless(status_parser.state_changed?)
+  
+  if(status_parser.build_running? && !status_parser.last_build_successful?)
+    AmpelSwitcher.green_on
+    AmpelSwitcher.red_on
+  elsif(status_parser.last_build_successful?)
+    AmpelSwitcher.green_on
+    AmpelSwitcher.red_off
   else
+    AmpelSwitcher.red_on
+    AmpelSwitcher.green_off
+  end
+end
+
+def set_ampel_error_state(status_parser)
+  if(status_parser.state_changed?)
     AmpelSwitcher.red_off
     AmpelSwitcher.green_off
+  end
+end
+
+loop do 
+  if(status_parser.update)
+    set_ampel_state(status_parser)
+  else
+    set_ampel_error_state(status_parser)
   end
   
   sleep(5)
